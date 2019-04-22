c69114fa0ad476e5b76cc3f2d4972e45e3a56048
go1dfish <me@go1dfish.me>
2019-04-21 21:03:36 -0700

    Update dep

--- dist/@notabug/peer.es5.js
+++ dist/@notabug/peer.es5.js
@@ -5,7 +5,7 @@ import { parse } from 'uri-js';
 import Route from 'route-parser';
 import memoize from 'fast-memoize';
 import * as R from 'ramda';
-import { compose, map, toPairs, trim, split, replace, defaultTo, nth, reduce, pathOr, test, assocPath, keys, without, keysIn, propOr, tap, uniqBy, values, mergeLeft, always, uniq, assoc, curry, prop, path, dissoc, difference, omit, slice, filter, sortWith, ascend, cond, isNil, T, identity, addIndex, indexBy, concat, apply, juxt, sortBy, includes, multiply, pipe, equals, of, find, pathEq, identical, last, lte, gte, match, mergeRight, pick, toLower, ifElse } from 'ramda';
+import { compose, map, toPairs, trim, split, replace, defaultTo, nth, reduce, pathOr, test, assocPath, keys, without, keysIn, propOr, tap, uniqBy, values, mergeLeft, always, uniq, assoc, curry, prop, path, dissoc, difference, omit, slice, filter, sortWith, ascend, cond, isNil, T, identity, addIndex, indexBy, concat, sortBy, includes, multiply, apply, juxt, pipe, equals, of, find, pathEq, identical, last, lte, gte, match, mergeRight, pick, toLower, ifElse } from 'ramda';
 import { query, resolve, scope, all } from '@notabug/gun-scope';
 
 /*! *****************************************************************************
