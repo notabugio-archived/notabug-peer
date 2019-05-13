6469ddf6daf66e1626c5f035ed9879e5278a8cf7
go1dfish <me@go1dfish.me>
2019-05-13 08:26:13 -0700

    Pagination fix

--- src/Listing/ListingFilter.ts
+++ src/Listing/ListingFilter.ts
@@ -214,7 +214,7 @@ const getFilteredRows = (
     );
 
   const fetchNextBatch = (): Promise<ListingNodeRow[]> => {
-    if (filtered.length > count || !rows.length) {
+    if (filtered.length > limit || !rows.length) {
       return resolve(
         R.compose(
           limit
