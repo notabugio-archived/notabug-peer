f8b01a5be29f0053b7f3fb7b40ee2a8c5ac779d0
go1dfish <me@go1dfish.me>
2019-11-16 19:50:40 +0000

    Remove GunNode from exports

--- src/@notabug/peer.ts
+++ src/@notabug/peer.ts
@@ -3,7 +3,7 @@ export { Config } from '../Config';
 export { Constants } from '../Constants';
 export { CommentCommand } from '../CommentCommand';
 export { Listing, SpaceSpec } from '../Listing';
-export { GunNode } from '../GunNode';
+// export { GunNode } from '../GunNode';
 export { Page } from '../Page';
 export { Peer } from '../Peer';
 export { Query } from '../Query';
